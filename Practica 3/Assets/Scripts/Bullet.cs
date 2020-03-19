712b8763f97876016e954936f95ecf35d9412bf8
Gabriela <gagegas@hotmail.com>
2020-03-19 01:51:15 -0600

    Agregar practica 3 crosshair

---
+++ Practica 3/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject decalPrefab;
+    private GameObject decalTMP;
+        private RaycastHit rayCast;
+    void OnCollisionEnter()
+    {
+       if(Physics.Raycast(transform.position, transform.forward, out rayCast, 10f)){
+                 decalTMP = Instantiate(decalPrefab, rayCast.point, Quaternion.identity);
+    Destroy(this.gameObject);
+                 Destroy(decalTMP, 10);
+                 }
+}
+void OnDrawGizmos(){
+    Gizmos.color = Color.red;
+    Gizmos.DrawRay(transform.position, transform.position * 10f);
+}
+}
